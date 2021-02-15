99ab8f15d8df607a9e697c92bd4eccd38ec03ebe
leticiafrontend <leticia.alesilva@outlook.com>
2021-02-15 15:52:52 -0300

    ✨ - add project structure

---
+++ src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+import StarWars from '../fonts/starjedi-webfont.woff';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Lato', Arial, Helvetica, sans-serif;
+  }
+
+  @font-face {
+    font-family: 'starwars';
+    src: url(${StarWars}) format('woff');
+  }
+`;
